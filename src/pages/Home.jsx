21f58bb56bbf20456a90ff84ad71132a6364a79e
Muddada Tejeswara Rao <lsandptlove143@gmail.com>
2023-09-29 23:16:24 +0530

    redirection between pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+        <div>click <Link to="/profile"><span className='hover:underline text-blue-500'>here</span></Link> to go to profile page</div>
+    </div>
   )
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Profile() {
   return (
-    <div>Profile</div>
+    <div>
+        <div>click <Link to="/"><span className='hover:underline text-blue-500'>here</span></Link> to go to home page</div>
+    </div>
   )
 }
