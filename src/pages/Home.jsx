b6066f1ec972f36e0e8cd62bf2785f569d1885f3
Muddada Tejeswara Rao <lsandptlove143@gmail.com>
2023-09-30 11:45:44 +0530

    redirecting to personal page is completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios';
 
 export default function Home() {
+  let url = "https://panorbit.in/api/users.json"
+  const [data,setData] = useState([]); 
+  const fetchData = async () => {
+    const d = await axios.get(url);
+    setData(d.data["users"]);
+  }
+  useEffect(() => {
+    fetchData()
+  },[])
+  const goToDetails = () => {
+    this.props.router.push('/profile');
+  };
   return (
     <div>
         <div>click <Link to="/profile"><span className='hover:underline text-blue-500'>here</span></Link> to go to profile page</div>
+        <div>
+          {data.map((object,i) => {
+            return (
+              <div className='flex' key={i} onClick={() => goToDetails()}>
+                <Link to="/profile" state={object}>
+                <img className='w-5 h-5 rounded-xl' src={object.profilepicture} key={i}/> 
+                <p>{object.name}</p>
+                </Link>
+              </div>
+            )
+          })}
+        </div>
     </div>
   )
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link ,useLocation} from 'react-router-dom'
 
 export default function Profile() {
+const location = useLocation()
+const data = location.state;
+console.log(data,"prop data in profile page")
   return (
     <div>
         <div>click <Link to="/"><span className='hover:underline text-blue-500'>here</span></Link> to go to home page</div>
+        <div>
+          <ul>
+            <li>{data.name}</li>
+          </ul>
+        </div>
     </div>
   )
 }
