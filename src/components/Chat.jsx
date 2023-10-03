6aa1df074023d6a0cbce3b1e1d7effcd95ee083f
Muddada Tejeswara Rao <lsandptlove143@gmail.com>
2023-10-03 11:15:07 +0530

    indivudual chat are getting deleted and wrapped up

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -7,11 +7,24 @@ import "./Chat.css"
 let nextId = 0
 
 export default function Chat({element,removeItem}) {
-    const [showindchat, setIndChat] = useState(false)
+    const [showindchat, setIndChat] = useState(true)
     const [clicked, setClicked] = useState(false)
 
     const [chattyped,setChattyped] = useState("");
     const [conversation, setConversation] = useState([])
+
+	// function enterPressed(event) {
+	// 	var code = event.keyCode || event.which;
+	// 	if(code === 13) { //13 is the enter keycode
+	// 		const newConvo = {
+	// 			id: nextId++,
+	// 			text : chattyped
+	// 		}
+	// 		setConversation([...conversation,newConvo])
+	// 		setChattyped('')
+	// 		console.log(conversation,"text entered")
+	// 	} 
+	// }
 
     let chats = ["hi","hellow","how are you"]
 	return (
@@ -19,21 +32,25 @@ export default function Chat({element,removeItem}) {
 			<div className="4w-full text-white bg-blue-600 p-5 flex items-center justify-between rounded-t-2xl border-x-blue">
 				<div className="flex items-center text-white">
 					<img className="w-8 h-8 rounded-full" src={element.profilepicture} alt={element.name}/>
-					<span className="ms-2">{element.name}</span>
+					<span className="ms-2 whitespace-nowrap maxwidth overflow-hidden text-ellipsis">{element.name}</span>
 				</div>
 				<div className="flex items-center">
-					<div className="me-2">
+					<div 
+						className="me-2 hover:cursor-pointer"
+						onClick={() => setIndChat(!showindchat)}
+					>
 						{showindchat === true ? <FaAngleDown /> : <FaAngleUp />}
 					</div>
 					<div
-                    onClick={() => removeItem(element)}
+						className="hover:cursor-pointer"
+                    	onClick={() => removeItem(element)}
                     >
 						<IoMdClose />
 					</div>
 				</div>
 			</div>
 			<div
-				className="block w-full overflow-y-scroll scrollbar max-h-80 me-2 border-x-blue"
+				className={showindchat === true?"bg-white block w-full overflow-y-scroll scrollbar max-h-48 me-2 border-x-blue":"hidden"}
 				
 			>
 				{conversation.map((ele, i) => {
@@ -49,9 +66,24 @@ export default function Chat({element,removeItem}) {
 					);
 				})}
 			</div>
-            <div className="bottom-0 flex justify-between items-center border border-x-blue pe-2">
-                <form>
-                    <input className="ps-4 focus:outline-none w-full rounded-md" value={chattyped} onChange={(e)=>setChattyped(e.target.value)} type="text" placeholder="Type here ..."/>
+            <div className={showindchat === true?"bottom-0 flex justify-between items-center border border-x-blue pe-2":"hidden"}>
+                <form onkeyp>
+                    <input 
+					// onChange={this.textChange.bind(this)}
+					// value={this.state.name} 
+					// onKeyPress={this.enterPressed.bind(this)}
+					// onKeyPress={
+					// 	(event) => {
+					// 		const newConvo = {
+					// 			id: nextId++,
+					// 			text : chattyped
+					// 		}
+					// 		setConversation([...conversation,newConvo])
+					// 		setChattyped('')
+					// 		console.log(conversation,"text entered")
+					// 	}
+					// }
+					 className="ps-4 focus:outline-none w-full rounded-md" value={chattyped} onChange={(e)=>setChattyped(e.target.value)} type="text" placeholder="Type here ..."/>
                 </form>
                 <IoMdSend onClick={(event) => {
                     const newConvo = {
